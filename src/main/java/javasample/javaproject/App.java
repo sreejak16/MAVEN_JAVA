2375d83c0650961c68f05e3862fb2badf1f2a310
sreejak16 <ksreeja1616@gmail.com>
2023-12-08 10:52:22 +0530

    javapush

---
+++ src/main/java/javasample/javaproject/App.java
@@ -0,0 +1,13 @@
+package javasample.javaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
